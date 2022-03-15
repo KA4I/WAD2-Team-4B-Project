8f69d64c12913f93f8d86ff63ef54828a5388c34
Petko Dimitrov <2523355D@student.gla.ac.uk>
2022-03-15 17:55:05 +0000

    Restore Correct Pet_Store Folder

---
+++ Pet_Store/populate_store.py
@@ -0,0 +1,51 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                        'pet_store_project.settings')
+
+import django
+django.setup()
+from store.models import Category, Product, Order, Review
+
+def populate():
+
+    toy_products = [{'uid':1,'name':'Squeaky Bone','description':'A toy bone that makes a sound!','price':2.99},
+                    {'uid':2,'name':'Ball and String','description':'Fun for all animals (but mostly cats)','price':4.99},
+                    {'uid':3,'name':'Cat Tower','description':'For cats to climb and make their home in.','price':10.99}]
+
+    animal_products = [{'uid':4,'name':'Lizard','description':'Perfect is you are after a more exotic pet ','price':29.99},
+                        {'uid':5,'name':'Ginger Cat','description':'Warning: This cat is known to eat tons of lasagne','price':39.99},
+                        {'uid':6,'name':'Hamster','description':'Hey hey hey, he is just a little guy and its his birthday','price':10.99}]
+
+    food_products = [{'uid':7,'name':'Canned Dog Food','description':'Meat chunks in gravy, not safe for human consumption','price':1.99},
+                    {'uid':8,'name':'Crickets','description':'A lizards favourite food, beware, they jump.','price':1.99},
+                    {'uid':9,'name':'Canned Cat Food','description':'Meat chunks in brine, not safe for human comsumption','price':1.99}]
+
+    cat = {'Toys':{'products':toy_products},
+            'Animals':{'products':animal_products},
+            'Food':{'products':food_products}}
+
+    for cat, cat_data in cat.items():
+        c = add_cat(cat)
+        for p in cat_data['products']:
+            add_product(c, p['name'], p['uid'], p['description'], p['price'])
+
+    for c in Category.objects.all():
+        for p in Product.objects.filter(category=c):
+            print(f'- {c}: {p}')
+
+
+def add_product(cat, name, uid, description, price=0.99):
+    p = Product.objects.get_or_create(category=cat, name=name, uid=uid)[0]
+    p.description=description
+    p.price=price
+    p.save()
+    return p
+
+def add_cat(name):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.save()
+    return c
+
+if __name__ == '__main__':
+    print('Starting Store population script...')
+    populate()
---
+++ Pet_Store/store/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    views = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args,**kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    uid = models.IntegerField(unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2, default=0.99)
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Product, self).save(*args,**kwargs)
+
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    address = models.CharField(max_length=255)
+    postcode = models.CharField(max_length=16)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    uid = models.IntegerField(unique=True)
+    review = models.CharField(max_length=225)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.review
+
+class Order(models.Model):
+    uid = models.IntegerField(unique=True)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    dateOrdered = models.DateTimeField()
+    arrival = models.DateField()
+
+    def __str__(self):
+        return self.uid
